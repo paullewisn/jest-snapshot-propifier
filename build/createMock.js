f2bcd3d53c9beb61eb36c000aebd09d5b9c9bd81
Paul-Lewis Nunn <paul-lewis.nunn@spotlightsportsgroup.com>
2022-03-21 16:36:29 +0000

    babel config

--- build/createMock.js
+++ build/createMock.js
@@ -13,6 +13,8 @@ var _recursivelyProcessProps = require("./utils/recursivelyProcessProps");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
 const Mock = ({
   $name,
   children,
@@ -28,10 +30,10 @@ const Mock = ({
 
   switch (element) {
     case "div":
-      return <div {...dataProps}>{childrenToRender}</div>;
+      return /*#__PURE__*/_react.default.createElement("div", dataProps, childrenToRender);
 
     case "span":
-      return <span {...dataProps}>{childrenToRender}</span>;
+      return /*#__PURE__*/_react.default.createElement("span", dataProps, childrenToRender);
 
     default:
       break;
@@ -42,6 +44,10 @@ const createMock = ({
   name,
   props,
   element
-}) => jest.fn((override = {}) => <Mock $name={name} {...props} {...override} element={element} />).mockName(name);
+}) => jest.fn((override = {}) => /*#__PURE__*/_react.default.createElement(Mock, _extends({
+  $name: name
+}, props, override, {
+  element: element
+}))).mockName(name);
 
 exports.createMock = createMock;
